14f3bd228e79f7e92d03731eddbc2051b5d3dd76
justcoding121 <jehonathant@gmail.com>
2020-04-10 21:39:54 -0600

    saves

--- src/Bible.Alarm/Bible.Alarm.iOS/Helpers/PnsService.cs
+++ src/Bible.Alarm/Bible.Alarm.iOS/Helpers/PnsService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -15,13 +16,14 @@ namespace Bible.Alarm.iOS.Helpers
     public class PnsService
     {
 #if DEBUG
-        private static readonly string url = "http://192.168.1.64:5010/api/v1/ScheduleAlarm";
+        private static readonly string url = "http://192.168.1.64:5010/api/v1/";
 #else
-        private static readonly string url = "https://production-push.jthomas.info/api/v1/ScheduleAlarm";
+        private static readonly string url = "https://production-push.jthomas.info/api/v1/";
 #endif
 
         public static async Task<HttpResponseMessage> RegisterDevice(string deviceId, string deviceToken)
         {
+            var apiUrl = Path.Combine(url, "RegisterDevice");
             var request = new DeviceRequest()
             {
                 DeviceId = deviceId,
@@ -33,7 +35,7 @@ namespace Bible.Alarm.iOS.Helpers
                 using var client = new HttpClient();
                 var payload = JsonConvert.SerializeObject(request);
                 HttpContent content = new StringContent(payload, Encoding.UTF8, "application/json");
-                return await client.PostAsync(url, content);
+                return await client.PostAsync(apiUrl, content);
 
             }, 3, true);
 
@@ -42,7 +44,7 @@ namespace Bible.Alarm.iOS.Helpers
         public static async Task<HttpResponseMessage> ScheduleAlarm(string deviceId, string deviceToken,
                         DateTime alarmTime, long notificationId)
         {
-
+            var apiUrl = Path.Combine(url, "ScheduleAlarm");
             var request = new ScheduleAlarmRequest()
             {
                 DeviceId = deviceId,
@@ -57,7 +59,7 @@ namespace Bible.Alarm.iOS.Helpers
                 using var client = new HttpClient();
                 var payload = JsonConvert.SerializeObject(request);
                 var content = new StringContent(payload, Encoding.UTF8, "application/json");
-                return await client.PostAsync(url, content);
+                return await client.PostAsync(apiUrl, content);
 
             }, 3, true);
 
@@ -66,6 +68,7 @@ namespace Bible.Alarm.iOS.Helpers
         public static async Task<HttpResponseMessage> RemoveAlarm(string deviceId,
                     string deviceToken, long notificationId)
         {
+            var apiUrl = Path.Combine(url, "CancelAlarm");
             var request = new CancelAlarmRequest()
             {
                 DeviceId = deviceId,
@@ -79,7 +82,7 @@ namespace Bible.Alarm.iOS.Helpers
                 using var client = new HttpClient();
                 var payload = JsonConvert.SerializeObject(request);
                 var content = new StringContent(payload, Encoding.UTF8, "application/json");
-                return await client.PostAsync(url, content);
+                return await client.PostAsync(apiUrl, content);
 
             }, 3, true);
         }
